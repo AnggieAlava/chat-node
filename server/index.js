feee01e86515e9ca96c4e979cfb39b568ee011c2
Anggie Alava <codewithangels@gmail.com>
2024-03-17 22:04:19 -0500

    Creado el server del chat con node

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('a user has connected!')
+
+  socket.on('disconnect ', () => {
+    console.log('an user has disconnected')
+  })
+})
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}...`)
+})
